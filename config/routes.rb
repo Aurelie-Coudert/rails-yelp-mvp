b4ef80d3a3c22bd67a0fefbfafe97177eff77fbe
Aurelie Coudert <aurelie.proffit@gmail.com>
2022-11-23 12:04:59 +0100

    models validation

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,17 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :restaurants do
+#   Prefix Verb   URI Pattern                            Controller#Action
+#   restaurants GET    /restaurants(.:format)                 restaurants#index
+#               POST   /restaurants(.:format)                 restaurants#create
+# new_restaurant GET    /restaurants/new(.:format)             restaurants#new
+# edit_restaurant GET    /restaurants/:id/edit(.:format)        restaurants#edit
+#    restaurant GET    /restaurants/:id(.:format)             restaurants#show
+#               PATCH  /restaurants/:id(.:format)             restaurants#update
+#               PUT    /restaurants/:id(.:format)             restaurants#update
+#               DELETE /restaurants/:id(.:format)             restaurants#destroy
+    resources :reviews, only: [:new, :create]
+  end
+  resources :reviews, only: [:destroy]
 end
